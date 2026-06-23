a0ae4fa0dd8cc28f2d4840a1e5a14af60b503177
Maximiliano Ahumada <maximiliano.ahumada@usach.cl>
2026-06-23 14:55:48 -0400

    fix

--- formula/cli-helper.rb
+++ formula/cli-helper.rb
@@ -7,17 +7,17 @@ class CliHelper < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/Maxahup/cli-command-helper/releases/download/v1.1.0/cli-helper-darwin-arm64"
-      sha256 57dec9fe6634fab046c1929bf3fb476757f9d385000ea28408a99cf9b619be46
+      sha256 "57dec9fe6634fab046c1929bf3fb476757f9d385000ea28408a99cf9b619be46"
     elsif Hardware::CPU.intel?
       url "https://github.com/Maxahup/cli-command-helper/releases/download/v1.1.0/cli-helper-darwin-amd64"
-      sha256 c6c6f6da7ef0df9fa5c65690b6c70c4010c49449e9700852cbf69c2404c844b7
+      sha256 "c6c6f6da7ef0df9fa5c65690b6c70c4010c49449e9700852cbf69c2404c844b7"
     end
 
   # SECCIÓN PARA LINUX
   elsif OS.linux?
     if Hardware::CPU.intel? # En la nomenclatura de Homebrew, intel? corresponde a x86_64 / amd64
       url "https://github.com/Maxahup/cli-command-helper/releases/download/v1.1.0/cli-helper-linux-amd64"
-      sha256 3cb82c24e78fc3c6a6a68b8f72ae996a4201220b53c4cad26220a129b0f2e6e2
+      sha256 "3cb82c24e78fc3c6a6a68b8f72ae996a4201220b53c4cad26220a129b0f2e6e2"
     end
   end
 
